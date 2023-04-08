18024ed2899ae9f9e78628da0f853ba252a313c7
navpannu32 <pannunav74@gmail.com>
2023-04-07 21:25:03 -0500

    add login page
    add login and logout functionality

---
+++ scripts/logout.php
@@ -0,0 +1,7 @@
+<?php
+  setcookie("name", "", time() - 3600, "/");
+  setcookie("email", "", time() - 3600, "/");
+  setcookie("role", "", time() - 3600, "/");
+  header('Location: ../index.php');
+  exit();
+?>
