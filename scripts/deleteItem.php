aa88cee9d93fdf3669da97e7c2d2ae90a9def9ce
navpannu32 <pannunav74@gmail.com>
2023-04-08 19:36:07 -0500

    moving files to there respective folders

--- scripts/deleteItem.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include '../database/connect.php';
-  $id = $_GET['id'];
-  $sql = 'DELETE FROM items WHERE id = :id;';
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute(['id' => $id]);
-  header('Location: ../index.php');
-?>
--- scripts/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-  setcookie("name", "", time() - 3600, "/");
-  setcookie("email", "", time() - 3600, "/");
-  setcookie("role", "", time() - 3600, "/");
-  header('Location: ../index.php');
-  exit();
-?>
