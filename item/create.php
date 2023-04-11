9e2f905fce6060e1f35a6caed584294835f37cad
navpannu32 <pannunav74@gmail.com>
2023-04-11 13:48:29 -0500

    add the functionality uploading image
    user can now upload a image of the item to a folder named iamge

--- item/create.php
+++ item/create.php
@@ -9,13 +9,15 @@
 <body>
   <?php require_once '../header.php'; ?>
   <h1>Create Item</h1>
-  <form action="../scripts/createItem.php" method="post">
+  <form action="../scripts/item/create.php" method="post" enctype="multipart/form-data">
     <label for="name">Name</label>
     <input type="text" name="name" id="name" required >
     <label for="description">Description</label>
     <input type="text" name="description" id="description" required>
     <label for="price">Price</label>
     <input type="number" name="price" id="price" required>
+    <label for="image">Image</label>
+    <input type="file" name="image" id="image">
     <button type="submit">Create</button>
   </form>
 </body>
--- scripts/item/create.php
+++ scripts/item/create.php
@@ -1,23 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <?php
-    include '../database/connect.php';
+<?php  
+  include '../../database/connect.php';
+  
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+  
+  $sql = 'INSERT INTO items (name, description, price) VALUES ("'.$name.'", "'.$description.'", '.$price.');';
+  
+  $stmt = $pdo->prepare($sql);
+  
+  if($stmt->execute()) {
+    if(isset($_FILES['image'])) {
+      $file = $_FILES['image'];
+      $fileName = $file['name'];
+      $fileTmpName = $file['tmp_name'];
+      $fileSize = $file['size'];
+      $fileError = $file['error'];
+      $fileType = $file['type'];
+      $fileExt = explode('.', $fileName);
+      $fileActualExt = strtolower(end($fileExt));
+      $allowed = array('jpg', 'jpeg', 'png');
+      if(in_array($fileActualExt, $allowed)) {
+        if($fileError === 0) {
+          if($fileSize < 1000000) {
+            $fileNameNew = uniqid('', true).".".$fileActualExt;
+            $fileDestination = '../../images/'.$fileNameNew;
 
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $price = $_POST['price'];
+            move_uploaded_file($fileTmpName, $fileDestination);
+            $sql = 'UPDATE items SET image = "'.$fileNameNew.'" WHERE id = '.$pdo->lastInsertId().';';
 
-    $sql = 'INSERT INTO items (name, description, price) VALUES ("'.$name.'", "'.$description.'", '.$price.');';
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    header('Location: ../index.php');
-  ?>
-</body>
-</html>
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+          } else {
+            echo "Your file is too big!";
+          }
+        } else {
+          echo "There was an error uploading your file!";
+        }
+      } else {
+        echo "You cannot upload files of this type!";
+      }
+    }
+  }
+  header('Location: ../../index.php');
+?>
