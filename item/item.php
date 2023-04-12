664a43825305e1c575abf7d682ff3f5c4c32e149
navpannu32 <pannunav74@gmail.com>
2023-04-12 13:36:15 -0500

    add classes for css

--- item/item.php
+++ item/item.php
@@ -9,33 +9,40 @@
 </head>
 <body>
   <?php require_once '../header.php'; ?>
-  <h1>Item</h1>
-  <?php
-    require_once '../database/connect.php';
-    $id = $_GET['id'];
-    $sql = 'SELECT * FROM items WHERE id = :id;';
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id' => $id]);
-    $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo '<h2>' . $item['name'] . '</h2>';
-    echo '<p>' . $item['description'] . '</p>';
-    echo '<p>' . $item['price'] . '</p>';
-    ?>
-    <form action="/restaurants/scripts/comment/create.php" method="post">
-      <label for="comment">Comment</label>
-      <input type="text" name="comment" id="comment" required>
-      <input type="hidden" name="item_id" value="<?php echo $id; ?>">
-      <input type="hidden" name="user_id" value="<?php echo $_COOKIE['id']; ?>">
-      <input type="submit" value="Create">
-    </form>
+  <div class="item-container">
+    <h1>Item</h1>
     <?php
-    $sql = 'SELECT * FROM comments WHERE item_id = :id;';
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id' => $id]);
-    $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($comments as $comment) {
-      echo '<p>' . $comment['comment'] . '</p>';
-    }
-  ?>
+      require_once '../database/connect.php';
+      $id = $_GET['id'];
+      $sql = 'SELECT * FROM items WHERE id = :id;';
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute(['id' => $id]);
+      $item = $stmt->fetch(PDO::FETCH_ASSOC);
+      echo '<h2>' . $item['name'] . '</h2>';
+      if($item['image']){
+        echo '<img src="../images/' . $item['image'] . '" alt="' . $item['name'] . '">';
+      }
+      echo '<p>' . $item['description'] . '</p>';
+      echo '<p>' . $item['price'] . '</p>';
+      ?>
+      <div class="comment-section">
+        <form action="/restaurants/scripts/comment/create.php" method="post">
+          <label for="comment">Comment</label>
+          <input type="text" name="comment" id="comment" required>
+          <input type="hidden" name="item_id" value="<?php echo $id; ?>">
+          <input type="hidden" name="user_id" value="<?php echo $_COOKIE['id']; ?>">
+          <input type="submit" value="Create">
+        </form>
+        <?php
+          $sql = 'SELECT * FROM comments WHERE item_id = :id;';
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute(['id' => $id]);
+          $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          foreach ($comments as $comment) {
+            echo '<p>' . $comment['comment'] . '</p>';
+          }
+        ?>
+    </div>
+  </div>
 </body>
 </html>
