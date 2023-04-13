3a993e108d7ae075d94c49690d6df1d5740cb7e6
navpannu32 <pannunav74@gmail.com>
2023-04-12 20:08:55 -0500

    add delete.php script
    delete item from database and delete the image related to it.

--- scripts/item/delete.php
+++ scripts/item/delete.php
@@ -1,8 +1,20 @@
 <?php
-  include '../../database/connect.php';
+  
+  require_once '../../database/connect.php';
   $id = $_GET['id'];
+  $sql = 'SELECT image FROM items WHERE id = :id;';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(['id' => $id]);
+  $image = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if($image['image']) {
+    unlink('../../images/' . $image['image']);
+  }
+
   $sql = 'DELETE FROM items WHERE id = :id;';
   $stmt = $pdo->prepare($sql);
   $stmt->execute(['id' => $id]);
-  header('Location: /restaurants/index.php');
+
+
+  header('Location: /restaurants/index.php');  
 ?>
