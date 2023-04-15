26e03d7d4cad8c0bae536abc1a28c87c5cd69550
navpannu32 <pannunav74@gmail.com>
2023-04-15 16:48:34 -0500

    create edit page for admin to edit item

--- admin/edit_item.php
+++ admin/edit_item.php
@@ -1,64 +1,46 @@
-<?php
-
-
-require_once '../database/connect.php';
-
-  if ($_COOKIE['role'] != "admin") {
-    header("Location: ../");
-    exit();
-  }
-  
-  $id = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');
-  
-  $sql = 'SELECT * FROM items WHERE id = ?;';
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute([$id]);
-  $donut = $stmt->fetch(PDO::FETCH_ASSOC);
-  if (!$donut) {
-    echo '<h1>Donut not found</h1>';
-    exit();
-  }
-  
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $price = $_POST['price'];
-    $image = $_POST['image'];
-    $sql = 'UPDATE donuts SET name = ?, description = ?, price = ?, image = ? WHERE id = ?;';
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$name, $description, $price, $image, $id]);
-    header("Location: /admin/items");
-    exit();
-  }
-
-  ?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/admin/item/edit.css">
-    <title>Edit Donut</title>
-  </head>
-  <body>
-  <?php include '../header.php'; ?>
-    
-    <h1>Edit Donut</h1>
-    
-  <form method="post">
-    <label for="name">Name:</label>
-    <input type="text" name="name" value="<?php echo $donut['name']; ?>" required>
-    <label for="description">Description:</label>
-    <textarea name="description" required><?php echo $donut['description']; ?></textarea>
-    <label for="price">Price:</label>
-    <input type="number" name="price" value="<?php echo $donut['price']; ?>" required>
-    <input type="submit" value="Save">
-  </form>
-
-
-  <?php include '.footer.php'; ?>
-
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/admin/item/edit.css">
+  <title>Edit Item</title>
+</head>
+<body>
+  <?php require_once '../header.php'; ?>
+  <?php
+    require_once '../database/connect.php';
+    $id = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8')  ?? 1;
+    $sql = 'SELECT * FROM items WHERE id = ?;';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+    $item = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$item) {
+      echo '<h1>Item not found</h1>';
+      exit();
+    }
+  ?>
+  <div class="edit-item">
+    <h1>Edit Item: <?php echo $item['name']; ?></h1>
+    <form action="/scripts/item/update.php" method="post" enctype="multipart/form-data">
+      <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" value="<?php echo $item['name']; ?>" required>
+      <label for="description">Description</label>
+      <textarea name="description" id="description" required><?php echo $item['description']; ?></textarea>
+      <label for="price">Price</label>
+      <input type="number" name="price" id="price" value="<?php echo $item['price']; ?>" required>
+      <label for="image">Image</label>
+      <?php if ($item['image']) { ?>
+        <img src="../images/<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>">
+        <label for="remove-image">Remove image</label>
+        <input type="checkbox" name="remove-image" id="remove-image">
+      <?php } ?>
+      <input type="file" name="image" id="image">
+      <input type="submit" value="Update">
+    </form>
+  </div>
+  <?php require_once '.footer.php'; ?>
 </body>
 </html>
--- item/edit.php
+++ item/edit.php
@@ -4,30 +4,43 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../styles/item/edit.css">
-  <title>Edit item</title>
+  <link rel="stylesheet" href="../styles/edit/edit.css">
+  <title>Edit Item</title>
 </head>
 <body>
+  <?php require_once '../header.php'; ?>
   <?php
     require_once '../database/connect.php';
     $id = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8')  ?? 1;
-    $sql = 'SELECT * FROM items WHERE id = :id;';
+    $sql = 'SELECT * FROM items WHERE id = ?;';
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id' => $id]);
+    $stmt->execute([$id]);
     $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    ?>
-  <?php require_once '../header.php'; ?>
-  
-  <h1>Edit item</h1>
-  <form action="../scripts/editItem" method="post">
-    <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" value="<?php echo $item['name']; ?>">
-    <label for="description">Description</label>
-    <input type="text" name="description" id="description" value="<?php echo $item['description']; ?>">
-    <label for="price">Price</label>
-    <input type="number" name="price" id="price" value="<?php echo $item['price']; ?>">
-    <button type="submit">Update</button>
-  </form>
+    if (!$item) {
+      echo '<h1>Item not found</h1>';
+      exit();
+    }
+  ?>
+  <div class="edit-item">
+    <h1>Edit Item: <?php echo $item['name']; ?></h1>
+    <form action="/scripts/item/update.php" method="post" enctype="multipart/form-data">
+      <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" value="<?php echo $item['name']; ?>" required>
+      <label for="description">Description</label>
+      <textarea name="description" id="description" required><?php echo $item['description']; ?></textarea>
+      <label for="price">Price</label>
+      <input type="number" name="price" id="price" value="<?php echo $item['price']; ?>" required>
+      <label for="image">Image</label>
+      <?php if ($item['image']) { ?>
+        <img src="../images/<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>">
+        <label for="remove-image">Remove image</label>
+        <input type="checkbox" name="remove-image" id="remove-image">
+      <?php } ?>
+      <input type="file" name="image" id="image">
+      <input type="submit" value="Update">
+    </form>
+  </div>
+  <?php require_once '.footer.php'; ?>
 </body>
 </html>
