8201b9c01be390a445bbd26ce7fa558fa78234bf
navpannu32 <pannunav74@gmail.com>
2023-04-20 04:08:40 -0500

    add about page

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="./styles/about.css">
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <main class="main"  >
+      <h1>About Us</h1>
+      <p>We are a company that specializes in making delicious donuts. Our team of bakers have many years of experience and are dedicated to creating the best tasting donuts in town.</p>
+      <p>We use only the highest quality ingredients and are committed to using locally sourced products whenever possible. Our donuts are made fresh every day, so you can always be sure that you are getting the freshest possible product.</p>
+
+    </main>
+    <video autoplay loop muted playsinline id="bg-video">
+      <source src="./images/landing-page.mp4" type="video/mp4">
+    </video>
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
