441e063cab3563c97bce36a68ff57e59474d33cc
navpannu32 <pannunav74@gmail.com>
2023-04-20 04:32:39 -0500

    fix edit.php for admin
    fix the path to the script to update the item

--- scripts/item/edit.php
+++ scripts/item/edit.php
@@ -2,14 +2,16 @@
 <?php
     include '../../database/connect.php';
 
-    $id = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');
+    $id = htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8');
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
-    $price = htmlspecialchars($_GET['price'], ENT_QUOTES, 'UTF-8');
+    $price = htmlspecialchars($_POST['price'], ENT_QUOTES, 'UTF-8');
 
     $image = $_POST['image'];
 
     $sql = 'UPDATE items SET name = "'.$name.'", description = "'.$description.'", price = '.$price.', image = "'.$image.'" WHERE id = '.$id.';';
+
+    echo $sql;
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
